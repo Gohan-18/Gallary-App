bc2357619424977c849a0f2f7806f0e51bf7d478
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2022-11-14 22:26:27 +0530

    updated

--- Gallary-App/src/home.js
+++ Gallary-App/src/home.js
@@ -4,6 +4,7 @@ const GET_IMG_URL = `https://api.unsplash.com/photos/random?client_id=${API_KEY}
 const search = document.querySelector("#input-field");
 const submitBtn = document.querySelector("#submit-btn");
 const imageField = document.querySelector("#main-field");
+
 let allImages;
 let searchTag;
 
@@ -13,9 +14,13 @@ search.addEventListener("change", (event) => {
 
 // const GET_IMG_URL_custom = `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`;`https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", async () => {
     console.log(searchTag)
-    fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
+
+    // let imgRenderField = document.querySelector("#gallary-class");
+    // imgRenderField.remove();
+
+    await fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
     .then(res => res.json())
     .then(data => {
         // allImages = data.hits;
@@ -37,12 +42,21 @@ submitBtn.addEventListener("click", () => {
 
 
 const makeImages = (data) => {
+
+    let imgRenderField = document.querySelector("#gallary-class");
+    imgRenderField.remove();
+
+    let gallaryClass = document.createElement('div');
+    gallaryClass.id = 'gallary-class';
+    imageField.appendChild(gallaryClass);
     data.map(data => {
         let img = document.createElement('img');
-        img.src = data.urls.small;
+        img.style.height = "350px";
+        img.style.width = "350px";
+        img.src = data.urls.regular;
         img.key = data.id;
         img.className = 'gallary-image';
-        imageField.appendChild(img);
+        gallaryClass.appendChild(img);
     })
 }
 
