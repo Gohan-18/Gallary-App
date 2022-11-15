821a0f69048ac0bd9b8281c06867aae947a6d174
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2022-11-15 12:18:37 +0530

    enter key implementation done

--- Gallary-App/src/home.js
+++ Gallary-App/src/home.js
@@ -1,6 +1,4 @@
 const API_KEY = "JIiTGGkMz0PshhK76-PKRRkM1VplhiViGoRJNYG9yUw";
-const API_KEY2 = "31203409-7714967bd984fca2e2e04f944";
-const GET_IMG_URL = `https://api.unsplash.com/photos/random?client_id=${API_KEY}&count=30`
 const search = document.querySelector("#input-field");
 const submitBtn = document.querySelector("#submit-btn");
 const imageField = document.querySelector("#main-field");
@@ -24,28 +22,14 @@ const fetchData = () => {
     });
 }
 
-// const GET_IMG_URL_custom = `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`;`https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`
-
 submitBtn.addEventListener("click", fetchData)
 
-// document.addEventListener("keydown" , (event) => {
-//     console.log(event.key);
-//     if(event.key === 'Enter') {
-        
-//     }
-// })
-
-
-// const makeImages = (data) => {
-//     data.map(data => {
-//         let img = document.createElement('img');
-//         img.src = data.largeImageURL;
-//         img.key = data.id;
-//         img.className = 'gallary-image';
-//         imageField.appendChild(img);
-//     })
-// }
-
+document.addEventListener("keyup" , (event) => {
+    console.log(event.key);
+    if(event.key === 'Enter') {
+        fetchData();
+    }
+})
 
 const makeImages = (data) => {
 
@@ -65,3 +49,32 @@ const makeImages = (data) => {
         gallaryClass.appendChild(img);
     })
 }
+
+// PixaBay API code below :-
+
+// const PIXABAY_API_KEY = "31203409-7714967bd984fca2e2e04f944";
+// const GET_IMG_URL_UNSPLASH = `https://api.unsplash.com/photos/random?client_id=${API_KEY}&count=30`
+
+// const GET_IMG_URL_custom = `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`;
+// `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`
+
+// const fetchData = () => {
+//     console.log(searchTag)
+//     fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
+//     .then(res => res.json())
+//     .then(data => {
+//         allImages = data.hits;
+//         console.log(allImages);
+//         makeImages(allImages);
+//     });
+// }
+
+// const makeImages = (data) => {
+//     data.map(data => {
+//         let img = document.createElement('img');
+//         img.src = data.largeImageURL;
+//         img.key = data.id;
+//         img.className = 'gallary-image';
+//         imageField.appendChild(img);
+//     })
+// }
