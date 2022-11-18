b287682f44fd763bfe4bcf2f8f80d9c27b47d9cd
Prabhat <112978778+Gohan-18@users.noreply.github.com>
2022-11-18 21:51:52 +0530

    First Version Completed

--- Gallary-App/src/home.js
+++ Gallary-App/src/home.js
@@ -12,7 +12,7 @@ document.addEventListener("keyup" , (event) => {
 })
 
 const fetchDefaultData = () => {
-    fetch(`https://api.unsplash.com/photos/random?clientid=${API_KEY}&count=30`)
+    fetch(`https://api.unsplash.com/photos/random?client_id=${API_KEY}&count=30`)
     .then(res => res.json())
     .then(data => {
         allImages = data;
@@ -64,71 +64,3 @@ const showPopup = (item) => {
     })
     img.src = item.urls.regular;
 }
-
-// Experiment code below
-
-// search.addEventListener("change", (event) => {
-//     searchTag = event.target.value;
-// })
-
-// const fetchData = () => {
-//     console.log(searchTag)
-//     fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
-//     .then(res => res.json())
-//     .then(data => {
-//         // allImages = data.hits;
-//         allImages = data.results;
-//         console.log(allImages);
-//         makeImagesSearch(allImages);
-//     });
-// }
-
-// searchBtn.addEventListener("click", fetchData)
-
-// const makeImagesSearch = (data) => {
-
-//     let imgRenderField = document.querySelector("#gallary-class");
-//     imgRenderField.remove();
-
-//     let gallaryClass = document.createElement('div');
-//     gallaryClass.id = 'gallary-class';
-//     imageField.appendChild(gallaryClass);
-//     data.map(data => {
-//         let img = document.createElement('img');
-//         img.style.height = "350px";
-//         img.style.width = "350px";
-//         img.src = data.urls.regular;
-//         img.key = data.id;
-//         img.className = 'gallary-image';
-//         gallaryClass.appendChild(img);
-//     })
-// }
-
-// PixaBay API code below :-
-
-// const PIXABAY_API_KEY = "31203409-7714967bd984fca2e2e04f944";
-// const GET_IMG_URL_UNSPLASH = `https://api.unsplash.com/photos/random?client_id=${API_KEY}&count=30`
-
-// const GET_IMG_URL_custom = `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`;
-// `https://pixabay.com/api?key=${API_KEY2}&q=${searchTag}`
-
-// const fetchData = () => {
-//     console.log(searchTag)
-//     fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
-//     .then(res => res.json())
-//     .then(data => {
-//         allImages = data.hits;
-//         console.log(allImages);
-//         makeImages(allImages);
-//     });
-// }
-
-// const makeImages = (data) => {
-//     data.map(data => {
-//         let img = document.createElement('img');
-//         img.src = data.largeImageURL;
-//         img.key = data.id;
-//         img.className = 'gallary-image';
-//         imageField.appendChild(img);
-//     })
-// }
--- Gallary-App/src/searchResult.js
+++ Gallary-App/src/searchResult.js
@@ -23,7 +23,6 @@ const fetchData = async () => {
         else{
             makeImagesSearch(allImages);
         }
-        // makeImagesSearch(allImages);
     })
     .catch(() => {
         let noDataFound = document.createElement('h3');
@@ -112,75 +111,3 @@ loadMoreBtn.addEventListener("click", () => {
     }
 
 })
-
-
-// const API_KEY = "JIiTGGkMz0PshhK76-PKRRkM1VplhiViGoRJNYG9yUw";
-// const search = document.querySelector("#input-field");
-// const searchBtn = document.querySelector("#search-btn-search-result");
-// const imageField = document.querySelector("#main-field");
-// let allImages;
-// let searchTag;
-
-// search.addEventListener("change", (event) => {
-//     searchTag = event.target.value;
-// })
-
-// const fetchData = () => {
-//     console.log(searchTag)
-//     fetch(`https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${searchTag}&per_page=30`)
-//     .then(res => res.json())
-//     .then(data => {
-//         // allImages = data.hits;
-//         allImages = data.results;
-//         console.log(allImages);
-//         makeImagesSearch(allImages);
-//     });
-// }
-
-// searchBtn.addEventListener("click", fetchData)
-
-// document.addEventListener("keyup" , (event) => {
-//     console.log(event.key);
-//     if(event.key === 'Enter') {
-//         fetchData();
-//     }
-// })
-
-// const makeImagesSearch = (data) => {
-
-//     let imgRenderField = document.querySelector("#gallary-class");
-//     imgRenderField.remove();
-
-//     let gallaryClass = document.createElement('div');
-//     gallaryClass.id = 'gallary-class';
-//     imageField.appendChild(gallaryClass);
-//     data.map((data, index) => {
-//         let img = document.createElement('img');
-//         img.style.height = "350px";
-//         img.style.width = "350px";
-//         img.src = data.urls.regular;
-//         img.key = data.id;
-//         img.className = 'gallary-image';
-//         gallaryClass.appendChild(img);
-
-//         img.addEventListener("click", () => {
-//             let currentImage = index;
-//             showPopup(data);
-//         })
-//     })
-// }
-
-// const showPopup = (item) => {
-//     let popup = document.querySelector(".image-popup");
-//     const downloadBtn = document.querySelector(".download-btn");
-//     const closeBtn = document.querySelector(".close-btn");
-//     const img = document.querySelector(".large-img");
-
-//     popup.classList.remove("hide-popup");
-//     downloadBtn.href = item.links.html;
-//     downloadBtn.target = "_blank";
-//     closeBtn.addEventListener("click", () => {
-//         popup.classList.add("hide-popup");
-//     })
-//     img.src = item.urls.regular;
-// }
